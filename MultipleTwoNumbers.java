a475aed3b2e539b0c0baebfcc7f8e6f0d2131bab
[Anusha139-anu] <[thakkurianusha139@gmail.com]>
2021-03-22 17:39:31 +0530

    committed

---
+++ MultipleTwoNumbers.java
@@ -0,0 +1,10 @@
+public class MultipleTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, mul;
+      mul = num1 * num2;
+
+      System.out.println("multiple of these numbers: "+mul);
+   }
+}
